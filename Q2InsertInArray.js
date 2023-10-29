64aa3524c020313c26cba887a6bbeec33e2c4a2f
buzz_404 <125798090+amandeepsirohi@users.noreply.github.com>
2023-10-29 10:52:37 +0530

    Update Q2InsertInArray.js
    added some base cases

--- Q2InsertInArray.js
+++ Q2InsertInArray.js
@@ -1,12 +1,21 @@
 let data = [56,566,3434,334,2343,22];
-    let newEl = 70;
-    let pos = 4;
+    let newEl = 999;
+    let pos = 3;
+   
+   //checks index out of range
+   if(data.length < pos || pos < 0)
+   {
+     console.log("position is out of range");
+     return;
+   }
    
     for(let i = data.length-1;i>=0;i--){
         if(i>=pos){
             data[i+1] = data[i];
             if(i==pos){
                 data[i] = newEl;
+                //if position found breakout of the loop to skip extra iterations
+                break;
             }
         }
     }
